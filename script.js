e358b2afcbaebc48d519fcdf36ae86553ecdb8be
cphollow <cphollow@ncsu.edu>
2026-09-18 04:36:49 -0400

    Adjust Connect tab linking

--- script.js
+++ script.js
@@ -16,16 +16,34 @@ const skillsPara = document.createElement("p");
 skills.addEventListener("click", () => {
   skills.classList.toggle("github-open");
   skillsPara.textContent = "Java | HTML / CSS | Javascript | C";
-  if (skillsPara.parentElement == skills) {
-    console.log(skills.childNodes);
-    skills.removeChild(skillsPara);
+  if (skillsPara.parentElement === skills) {
+    setTimeout(() => {
+      skills.removeChild(skillsPara);
+    }, 100);
     return;
   }
+
   skills.appendChild(skillsPara);
 });
 
+//         <a href="https://www.linkedin.com/in/carmellaholloway/">Connect with me on Linkedin</a>
+
+const link = document.createElement("a");
+
 linkedin.addEventListener("click", () => {
   linkedin.classList.toggle("linkedin-open");
+  link.setAttribute("href", "https://www.linkedin.com/in/carmellaholloway/");
+  link.textContent = "Connect with me on Linkedin";
+
+   if (link.parentElement === linkedin) {
+    setTimeout(() => {
+      linkedin.removeChild(link);
+    }, 100);
+    return;
+  }
+
+  linkedin.appendChild(link);
+
 });
 
 const objecPara = document.createElement("p");
@@ -33,9 +51,10 @@ const objecPara = document.createElement("p");
 objective.addEventListener("click", () => {
   objective.classList.toggle("about-open");
   objecPara.textContent = "Develop products with a positive impact";
-  if (objecPara.parentElement == objective) {
-    console.log(objective.childNodes);
+  if (objecPara.parentElement === objective) {
+   setTimeout(() => {
     objective.removeChild(objecPara);
+    }, 100);
     return;
   }
   objective.appendChild(objecPara);
